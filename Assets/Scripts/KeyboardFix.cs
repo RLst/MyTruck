ad671212eca2aec2203b629f839b9a7e90b4bff8
Tony Le <letony@icloud.com>
2023-06-17 19:18:22 +0700

    Attempt to fix mobile device keyboard issue

---
+++ Assets/Scripts/KeyboardFix.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Runtime.InteropServices;
+using System;
+using TMPro;
+
+namespace hYu
+{
+    public class KeyboardFix : MonoBehaviour, ISelectHandler
+    {
+
+        [DllImport("__Internal")]
+        private static extern void focusHandleAction(string _name, string _str);
+
+        public void ReceiveInputData(string value)
+        {
+            gameObject.GetComponent<TMP_InputField>().text = value;
+        }
+
+        public void OnSelect(BaseEventData data)
+        {
+#if UNITY_WEBGL
+            try
+            {
+                focusHandleAction(gameObject.name, gameObject.GetComponent<TMP_InputField>().text);
+            }
+            catch (Exception error) { }
+#endif
+        }
+    }
+}
--- Assets/Scripts/SoundPerformer.cs
+++ Assets/Scripts/SoundPerformer.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace hYu
 {
-
     [RequireComponent(typeof(AudioSource))]
     public class SoundPerformer : Performer
     {
